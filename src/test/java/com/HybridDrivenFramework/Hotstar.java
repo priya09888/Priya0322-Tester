ed40a4836ab68e2add3d3ceaac3cb487db367b70
priya09888 <priyankapawar0322@gmail.com>
2026-04-05 17:19:32 +0530

    version 1.2

--- src/test/java/com/HybridDrivenFramework/Hotstar.java
+++ src/test/java/com/HybridDrivenFramework/Hotstar.java
@@ -1,22 +1,22 @@
-package com.HybridDrivenFramework;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-public class Hotstar {
-
-	public static void main(String[] args) throws Exception {
-
-		
-		FileInputStream fis = new FileInputStream("./src/test/resources/Hotstar.xlsx");
-		XSSFWorkbook work = new XSSFWorkbook(fis);
-		XSSFSheet sheet = work.getSheet("Sheet1");
-		for(i=1; )
-		
-		
-		
-	}
-
-}
+//package com.HybridDrivenFramework;
+//
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//
+//public class Hotstar {
+//
+//	public static void main(String[] args) throws Exception {
+//
+//		
+//		FileInputStream fis = new FileInputStream("./src/test/resources/Hotstar.xlsx");
+//		XSSFWorkbook work = new XSSFWorkbook(fis);
+//		//XSSFSheet sheet = work.getSheet("Sheet1");
+//		//for(i=1; )
+//		
+//		
+//		
+//	}
+//
+//}
--- src/test/java/com/HybridDrivenFramework/RepoOfHotstar.java
+++ src/test/java/com/HybridDrivenFramework/RepoOfHotstar.java
@@ -1,53 +1,53 @@
-package com.HybridDrivenFramework;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-public class RepoOfHotstar {
-	
-	public RepoOfHotstar(WebDriver w) {
-		PageFactory.initElements(w, this);
-	}
-	@FindBy(xpath= "//*[@class=\"_3O2khXNePOp8a3Gd1EocHu absolute h-full flex items-center ELEVATION_5\"]/nav/div[2]/a/button")
-	private WebElement searchicon;
-	
-	@FindBy(id= "searchBar")
-	private WebElement searchTextBar;
-	
-	@FindBy(xpath= "//*[@class=\"_3O2khXNePOp8a3Gd1EocHu absolute h-full flex items-center ELEVATION_5\"]/nav/div[8]/a/button")
-	private WebElement crossIconInsearchTextBar;
-	
-    @FindBy(xpath= "")
-    private WebElement myspaceIcon;
-    
-    @FindBy()
-    private WebElement loginbtn;
-    
-    @FindBy()
-    private WebElement mobileNumberField;
-    
-    @FindBy()
-    private WebElement getOTP;
-    
-    public void searchIcon() {
-    	searchicon.click();
-    }
-    public void searchTextBox() {
-    	searchTextBox
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
+//package com.HybridDrivenFramework;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//import org.openqa.selenium.support.PageFactory;
+//
+//public class RepoOfHotstar {
+//	
+//	public RepoOfHotstar(WebDriver w) {
+//		PageFactory.initElements(w, this);
+//	}
+//	@FindBy(xpath= "//*[@class=\"_3O2khXNePOp8a3Gd1EocHu absolute h-full flex items-center ELEVATION_5\"]/nav/div[2]/a/button")
+//	private WebElement searchicon;
+//	
+//	@FindBy(id= "searchBar")
+//	private WebElement searchTextBar;
+//	
+//	@FindBy(xpath= "//*[@class=\"_3O2khXNePOp8a3Gd1EocHu absolute h-full flex items-center ELEVATION_5\"]/nav/div[8]/a/button")
+//	private WebElement crossIconInsearchTextBar;
+//	
+//    @FindBy(xpath= "")
+//    private WebElement myspaceIcon;
+//    
+//    @FindBy()
+//    private WebElement loginbtn;
+//    
+//    @FindBy()
+//    private WebElement mobileNumberField;
+//    
+//    @FindBy()
+//    private WebElement getOTP;
+//    
+//    public void searchIcon() {
+//    	searchicon.click();
+//    }
+//    public void searchTextBox() {
+//    	//searchTextBox
+//    }
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//}
